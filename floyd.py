b19df3427e9cca996af30ee92d8d6b4f25bcdb08
ChakrishB <112625243+ChakrishB@users.noreply.github.com>
2022-09-08 12:51:55 +0530

    Add files via upload

---
+++ floyd.py
@@ -0,0 +1,23 @@
+# Python3 program to print
+# Floyd's triangle
+
+def floydTriangle(n):
+ 
+
+    val = 1
+
+    for i in range(1, n + 1):
+ 
+
+        for j in range(1, i + 1):
+
+            print(val, end = " ")
+
+            val += 1
+
+         
+
+        print("")
+ 
+
+floydTriangle(6)
